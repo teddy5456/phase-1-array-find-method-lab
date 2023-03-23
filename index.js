0b46cdfb78463884c3dc9902ecaa87615ab7caa2
Teddy <teddywambua5456@gmail.com>
2023-03-23 08:18:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(entry => entry.result === "W");
+    if (!winningRecord) {
+      return undefined;
+    }
+    return winningRecord.year;
+  }
